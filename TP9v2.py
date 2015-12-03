82f89e632ed98ff8162729c2f4bc82ac0cff70ed
RagnvaldArnulfson <gus683@hotmail.fr>
2015-12-03 20:45:35 +0100

    Update TP9v2.py

--- TP9v2.py
+++ TP9v2.py
@@ -4,17 +4,12 @@ l1=[r.randint(-10,10) for i in range(10**3)]
 
 def recurs(l):
     n=len(l)
-    
     if n==1:
         return l[0]
     
     k=(n//2)    
-    a1=recurs(l[0:k])
-    a2=recurs(l[k:n])
-    
     left_sum,right_sum = l[k-1],l[k]
     s_g,s_d=0,0
-
     for i in range(k,n):
         s_d+=l[i]
         if s_d < right_sum:
@@ -24,7 +19,7 @@ def recurs(l):
         if s_g < left_sum:
             left_sum = s_g
     
-    return min(a1,a2,left_sum+right_sum)
+    return min(recurs(l[0:k]),recurs(l[k:n]),left_sum+right_sum)
 
 def lineaire(l):
     c=l[0]
