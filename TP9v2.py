0a18205776a1cfe22246d6eac0ec156b6c971099
RagnvaldArnulfson <gus683@hotmail.fr>
2015-12-03 19:59:06 +0100

    Update TP9v2.py

--- TP9v2.py
+++ TP9v2.py
@@ -8,15 +8,11 @@ def recurs(l):
     if n==1:
         return l[0]
     
-    k=(n//2)
-    
+    k=(n//2)    
     a1=recurs(l[0:k])
     a2=recurs(l[k:n])
     
     left_sum,right_sum = l[k-1],l[k]
-    
-    i0,j0=0,0
-    
     s_g,s_d=0,0
 
     for i in range(k,n):
@@ -24,11 +20,8 @@ def recurs(l):
         s_g+=l[n-i-1-(n%2)]
         if s_d < right_sum:
             right_sum = s_d
-            j0 = i
         if s_g < left_sum:
             left_sum = s_g
-            i0 = i
-            
     
     return min(a1,a2,left_sum+right_sum)
 
